3c35bdd9bce42a8ea963a70d4defbb6ebe245f43
codypscott <cscott87@uncc.edu>
2022-02-25 21:15:43 -0500

    updated dataprep, analysis, and sprint 3

--- DataAnalysis.R
+++ DataAnalysis.R
@@ -1,11 +1,13 @@
 # install.packages("tidyverse", dependencies = TRUE)
-#install.packages("car", dependencies=TRUE)
+# install.packages("car", dependencies=TRUE)
 # install.packages("olsrr", dependencies=TRUE)
 
 library(tidyverse)
 library(car)
 
+## full data 
 
+## need to get relative path for this from repository
 full_data <- read.csv("C:\\Users\\ricke\\OneDrive\\Desktop\\DTSC4301A\\DTSC4301\\Dataprep\\Full_Data.csv")
 
 
@@ -17,14 +19,11 @@ names(full_data)
 
 ## hypothesis 1 data
 H1_data <- full_data[c('int_commcoll', 'int_pubsr', 'AP_part_pct', 'AP_pass_pct', 'enroll_Disadvantaged')]
-
-## hypothesis 2 data
-H2_data <-full_data[c('CTE_enroll_pct', 'CTE_cred_pct', 'int_trdbusnrs' )]
-
-
-
 cor(na.omit(H1_data))
 
 
+
+## hypothesis 2 data
+H2_data <-full_data[c('CTE_enroll_pct', 'CTE_cred_pct', 'int_trdbusnrs' )]
 cor(na.omit(H2_data))
 
